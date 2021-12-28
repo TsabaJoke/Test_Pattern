69b2043bcc11b04cf883eec35e52f694d12f5a8b
TsabaJoke <73981652+TsabaJoke@users.noreply.github.com>
2021-12-28 17:46:50 +0700

    Commit_2
    Новый код

--- Facade_P.py
+++ Facade_P.py
@@ -1,39 +1,70 @@
-"""Facade pattern WashingMachine"""
+import argparse
+import requests
 
-class Washing: #Стирает
-    '''Subsystem # 1'''
-
-    def wash(self):
-        print("Washing...")
-
-class Rinsing: #Полоскание
-    '''Subsystem # 2'''
-
-    def rinse(self):
-        print("Rinsing...")
-
-class Spinning: # Отжим
-    '''Subsystem # 3'''
-
-    def spin(self):
-        print("Spinning...")
+BASE_URL = "https://api.github.com"
 
 
-class WashingMachine:
-    '''Facade'''
+def generate_changelog(owner, repo, version):
+    github = GitHubClient()
+    release_dt = github.get_release_date(owner, repo, version)
+    commit_messages = github.get_commit_messages(owner, repo, release_dt)
 
-    def __init__(self):
-        self.washing = Washing()
-        self.rinsing = Rinsing()
-        self.spinning = Spinning()
+    changelog = ["CHANGELOG", ""]
+    for message in commit_messages:
+        changelog.append(f"- {message}")
+    return changelog
 
-    def startWashing(self):
-        self.washing.wash()
-        self.rinsing.rinse()
-        self.spinning.spin()
 
-""" main method """
+class GitHubClient:
+    """Facade around GitHub REST API"""
+
+    def get_release_date(self, owner, repo, version):
+        url = f"{BASE_URL}/repos/{owner}/{repo}/releases/tags/{version}"
+        resp = requests.get(url)
+        if resp.status_code == 404:
+            raise ValueError("Version does not exist")
+        resp.raise_for_status()
+
+        return resp.json()["published_at"]
+
+    def get_commit_messages(self, owner, repo, release_dt):
+        url = f"{BASE_URL}/repos/{owner}/{repo}/commits"
+        params = {"sha": "master", "since": release_dt}
+        resp = requests.get(url, params=params)
+        resp.raise_for_status()
+
+        messages = [item.get("commit", {}).get("message") for item in resp.json()]
+        return messages[::-1]
+
+
+def parse_args():
+    description = "Generate changelog for repository"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-r",
+        "--repo",
+        type=str,
+        help="Full path to repository, (abc/xyz)",
+        required=True,
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        help="Version to generate CHANGELOG from",
+        required=True,
+    )
+    return vars(parser.parse_args())
+
+
 if __name__ == "__main__":
+    args = parse_args()
+    try:
+        owner, repo = args["repo"].split("/")
+    except ValueError:
+        raise ValueError("Invalid repo")
+    version = args["version"]
 
-    washingMachine = WashingMachine()
-    washingMachine.startWashing()
+    changelog = generate_changelog(owner, repo, version)
+    print()
+    print("\n".join(changelog))
